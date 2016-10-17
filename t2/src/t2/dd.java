c3aad08736cb0549196625666d3c7bc92c55c937
yimi78 <477641103@qq.com>
2016-10-17 21:18:20 +0800

    1

--- t2/src/t2/dd.java
+++ t2/src/t2/dd.java
@@ -1,23 +1,23 @@
 /**   
  * Copyright © 2016 酷玛. All rights reserved.
  * 
- * @Title: yy.java 
- * @Prject: hello
- * @Package: hello 
+ * @Title: dd.java 
+ * @Prject: t2
+ * @Package: t2 
  * @Description: TODO
  * @author: lhq   
- * @date: Oct 17, 2016 9:10:01 PM 
+ * @date: Oct 17, 2016 9:17:20 PM 
  * @version: V1.0   
  */
-package hello;
+package t2;
 
 /** 
- * @ClassName: yy 
+ * @ClassName: dd 
  * @Description: TODO
  * @author: lhq
- * @date: Oct 17, 2016 9:10:01 PM  
+ * @date: Oct 17, 2016 9:17:20 PM  
  */
-public class yy {
+public class dd {
 
 	/** 
 	 * @Title: main 
