961fc43e09121747ef8419e74d78a4739be9b48c
yimi78 <477641103@qq.com>
2016-10-17 21:24:00 +0800

    # WARNING: head commit changed in the meantime
    ljksd

--- t2/src/t2/dd.java
+++ t2/src/t2/dd.java
@@ -11,23 +11,23 @@
  */
 package t2;
 
-/** 
- * @ClassName: dd 
+/**
+ * @ClassName: dd
  * @Description: TODO
  * @author: lhq
- * @date: Oct 17, 2016 9:17:20 PM  
+ * @date: Oct 17, 2016 9:17:20 PM
  */
 public class dd {
 
-	/** 
-	 * @Title: main 
+	/**
+	 * @Title: main
 	 * @Description: TODO
 	 * @param args
 	 * @return: void
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String ss = "1";
 	}
 
 }
