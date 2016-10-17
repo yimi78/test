bc5ae9b89ea192947ec9dbc3983e505e2a21add9
yimi78 <477641103@qq.com>
2016-10-17 21:10:40 +0800

    r

---
+++ hello/src/hello/yy.java
@@ -0,0 +1,33 @@
+/**   
+ * Copyright © 2016 酷玛. All rights reserved.
+ * 
+ * @Title: yy.java 
+ * @Prject: hello
+ * @Package: hello 
+ * @Description: TODO
+ * @author: lhq   
+ * @date: Oct 17, 2016 9:10:01 PM 
+ * @version: V1.0   
+ */
+package hello;
+
+/** 
+ * @ClassName: yy 
+ * @Description: TODO
+ * @author: lhq
+ * @date: Oct 17, 2016 9:10:01 PM  
+ */
+public class yy {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
