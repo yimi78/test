b67419f7b4db2877b2daaa733d1b92c65de12281
yimi78 <477641103@qq.com>
2017-02-26 17:17:15 +0800

    www

--- t4/src/ccc.java
+++
@@ -1,9 +0,0 @@
-
-public class ccc {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       int a="0";
-	}
-
-}
--- t4/src/dsfsdfsdf.java
+++ t4/src/dsfsdfsdf.java
@@ -3,8 +3,8 @@ public class dsfsdfsdf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int aaa="0";
-		int ss = "";
+String aaa="0";
+String ss = "";
 	}
 
 }
