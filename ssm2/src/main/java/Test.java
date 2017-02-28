1fb3de299d9d78aefbb0eade5adebdf66cfa5841
yimi78 <477641103@qq.com>
2017-02-28 11:27:40 +0800

    添加测试文件

---
+++ ssm2/src/main/java/Test.java
@@ -0,0 +1,32 @@
+/**   
+ * Copyright © 2017 酷玛. All rights reserved.
+ * 
+ * @Title: Test.java 
+ * @Prject: ssm2
+ * @Package:  
+ * @Description: TODO
+ * @author: lhq   
+ * @date: 2017年2月28日 上午11:26:48 
+ * @version: V1.0   
+ */
+
+/** 
+ * @ClassName: Test 
+ * @Description: TODO
+ * @author: lhq
+ * @date: 2017年2月28日 上午11:26:48  
+ */
+public class Test {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
