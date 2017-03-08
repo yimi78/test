8572d4179381af7d96ee49cd5b62a31ad056fc07
yimi78 <477641103@qq.com>
2017-03-08 16:28:27 +0800

    添加模块

--- ssm2/src/main/java/com/lhq/ssm2/service/sms_sendService.java
+++
@@ -1,9 +0,0 @@
-package com.lhq.ssm2.service;
-
-import org.springframework.stereotype.Service ;
-
-@ Service( "sms_sendService" )
-public interface sms_sendService {
-	 // 短信验证码
-	 public abstract String sms_send( String name , String pwd , String tel ) ;
-}
